6e1ce542bd816e7572665358de673412795aeace
Silverfangs <silverfangs@users.noreply.github.com>
2018-06-09 16:53:45 +0800

    Updated

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -21,6 +21,8 @@ export const HomePageTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="content">
+
+              {/* Hero banner */}
               <div
                 className="full-width-image-container margin-top-0"
                 style={{ backgroundImage: `url(${image})` }}
@@ -37,66 +39,20 @@ export const HomePageTemplate = ({
                   {title}
                 </h2>
               </div>
-              <div className="columns">
-                <div className="column is-7">
+
+              <div className="columns is-centered">
+                <div className="column is-10">
                   <h3 className="has-text-weight-semibold is-size-2">
                     {heading}
                   </h3>
                   <p>{description}</p>
                 </div>
               </div>
-              <Features gridItems={intro.blurbs} />
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-3">
-                    {main.heading}
-                  </h3>
-                  <p>{main.description}</p>
-                </div>
-              </div>
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical">
-                  <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image1.image}
-                          alt={main.image1.alt}
-                        />
-                      </article>
-                    </div>
-                    <div className="tile is-parent">
-                      <article className="tile is-child">
-                        <img
-                          style={{ borderRadius: '5px' }}
-                          src={main.image2.image}
-                          alt={main.image2.alt}
-                        />
-                      </article>
-                    </div>
-                  </div>
-                  <div className="tile is-parent">
-                    <article className="tile is-child">
-                      <img
-                        style={{ borderRadius: '5px' }}
-                        src={main.image3.image}
-                        alt={main.image3.alt}
-                      />
-                    </article>
-                  </div>
-                </div>
-              </div>
-              <Testimonials testimonials={testimonials} />
-              <div
+              {/* <div
                 className="full-width-image-container"
                 style={{ backgroundImage: `url(${fullImage})` }}
-              />
-              <h2 className="has-text-weight-semibold is-size-2">
-                {pricing.heading}
-              </h2>
-              <p className="is-size-5">{pricing.description}</p>
-              <Pricing data={pricing.plans} />
+              /> */}
+              <Testimonials testimonials={testimonials} />
             </div>
           </div>
         </div>
